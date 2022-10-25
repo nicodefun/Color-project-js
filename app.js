d34dbc88f12d99add5c60a2fff506c028d9e1716
Yonnie Yang <nicodefunyang@gmail.com>
2022-10-25 13:06:33 +0800

    slider colorized

--- app.js
+++ app.js
@@ -26,6 +26,15 @@ const randomColorDivs = () =>{
     divTextTag.innerText = randomColor;
     div.style.backgroundColor = randomColor;
     checkColorContrast(randomColor, divTextTag);
+
+    //initial colorize slider
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll('.sliders input');
+    // console.log(sliders);
+    const sliderHue = sliders[0];
+    const sliderBright = sliders[1];
+    const sliderSat = sliders[2];
+    colorizeSliders(color, sliderHue, sliderBright, sliderSat);
   })
 } 
 
@@ -36,6 +45,21 @@ const checkColorContrast = (color, text) => {
   }else{
     text.style.color = 'white';
   }
+}
+const colorizeSliders = (color, sliderHue, sliderBright, sliderSat) =>{
+  //goal get color min/max for each properties
+  //scale sat
+  const noSat = color.set('hsl.s', 0);
+  const fullSat = color.set('hsl.s', 1);
+  const scaleSat = chroma.scale([noSat, color, fullSat]);
+  //scale brightness
+  const midBright = color.set('hsl.l', 0.5);
+  const scaleBright = chroma.scale(['black', midBright, 'white']);
+
+  //input update --- important
+  sliderSat.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)}, ${scaleSat(1)})`;
+  sliderBright.style.backgroundImage = `linear-gradient(to right,  ${scaleBright(0)}, ${scaleBright(0.5)}, ${scaleBright(1)})`;
+  sliderHue.style.backgroundImage = `linear-gradient(to right, rgb(204, 75, 75), rgb(204,204 ,75),rgb(75, 204, 75),rgb(75, 204, 204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`
 }
 
 randomColorDivs();
